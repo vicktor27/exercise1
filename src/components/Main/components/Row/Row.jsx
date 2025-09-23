2f8abd004053f39346013afda335836819210e81
Vickthor3T <victor.cardoza@tripleten-team.com>
2025-09-23 12:07:13 -0600

    children

--- src/components/Main/components/Row/Row.jsx
+++ src/components/Main/components/Row/Row.jsx
@@ -1,12 +1,18 @@
+<<<<<<< HEAD
 import CopyRight from "./CopyRight";
 
+=======
+>>>>>>> ae58357 (children)
 function Row() {
   return (
     <>
       <p>Simulando un Renglon</p>
       <p>Simulando un Renglon</p>
       <p>Simulando un Renglon</p>
+<<<<<<< HEAD
       <CopyRight />
+=======
+>>>>>>> ae58357 (children)
     </>
   );
 }
