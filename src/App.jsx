e266a87cc6fb89d28808dcd873cd10342ebd45ec
Vickthor3T <victor.cardoza@tripleten-team.com>
2025-09-23 12:36:47 -0600

    asd

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import Header from "./components/Header/Header";
 import Footer from "./Footer";
 import MyName from "./MyName";
+import { useState } from "react";
 
 function App() {
   let name = "PhD Victor M Solis Cardoza";
-
+  const [x1, setX1] = useState("START VALUE");
   return (
     <>
       <Header />
       <MyName />
-      <Footer />
+      <Footer setX1={setX1} />
     </>
   );
 }
--- src/Header.jsx
+++ src/Header.jsx
@@ -2,6 +2,11 @@ function Header() {
   return (
     <header>
       <h1>Titulo Header</h1>
+      <button
+        onClick={() => {
+          setX1("EL NUEVO VALOR");
+        }}
+      ></button>
     </header>
   );
 }
