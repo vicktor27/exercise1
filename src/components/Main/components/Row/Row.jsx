706696602c3a8726c10b1e7d31e3f352fe11a4e8
Vickthor3T <victor.cardoza@tripleten-team.com>
2025-12-02 13:11:44 -0600

    dsa

--- src/components/Main/components/Row/Row.jsx
+++ src/components/Main/components/Row/Row.jsx
@@ -1,29 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-import CopyRight from "./CopyRight";
-
-=======
->>>>>>> ae58357 (children)
-=======
-import CopyRight from "./CopyRight";
-
->>>>>>> f2518a6 (BREAKING CHANGE: initial commit)
-function Row() {
-  return (
-    <>
-      <p>Simulando un Renglon</p>
-      <p>Simulando un Renglon</p>
-      <p>Simulando un Renglon</p>
-<<<<<<< HEAD
-<<<<<<< HEAD
-      <CopyRight />
-=======
->>>>>>> ae58357 (children)
-=======
-      <CopyRight />
->>>>>>> f2518a6 (BREAKING CHANGE: initial commit)
-    </>
-  );
-}
+function Row() {}
 
 export default Row;
